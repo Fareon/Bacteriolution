3f1fc252542b24028dd8a3519b0125f2e9c131a0
valeryparfenov <parfenov.vr@phystech.edu>
2021-11-22 19:46:27 +0300

    Теперь функция не создаёт ненужных массивов при первом вызове.

--- scene.py
+++ scene.py
@@ -1,4 +1,4 @@
-def pixel_blit(screen, objects=[], camera_pos=[], screen_param=[], pixel_view_amount=[]):
+def pixel_blit(screen, objects, camera_pos, screen_param, pixel_view_amount):
     '''
     we chose color for pixels, that we want to draw (pixels in camera area)
     and then draw them
