58ad6e35b317e1c6e7bdcd221f7838eefea55a52
Daniel <bdfaraon@gmail.com>
2021-11-23 12:43:50 +0300

    module init

---
+++ map.py
@@ -0,0 +1,11 @@
+class Map:
+    """
+    Gathers the information about objects in order to draw them after
+    """
+
+    def update(self):
+        """
+        Updates the data according to objects in play_units.py
+        :return:
+        """
+        pass
