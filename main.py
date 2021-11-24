d287ac46ffb4a9bc85af032b6363e9d76daa90f9
valeryparfenov <parfenov.vr@phystech.edu>
2021-11-24 21:32:17 +0300

    добавлена подсказка для вызова scene_display() (функция отрисовки сцены)

--- main.py
+++ main.py
@@ -28,5 +28,39 @@ def main():
         time.sleep(1.0 / FPS)
 
 
+# подсказка для вызова scene_display()
+'''
+pg.init() # просто делаем экранчик
+width = 400
+height = 600
+screen = pg.display.set_mode((width, height))
+
+
+class Drawble_bjects():  # класс отрисовываемых объектов (тут можно подсмотреть необходимые атрибуты)
+    def __init__(self, x, y, r, color=[]):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.r = r
+
+
+obj1 = Drawble_bjects(20, 25, 2, (0, 0, 0))  # создаём экземпляры и формируем список из них
+obj2 = Drawble_bjects(0, 40, 4, (56, 153, 203))
+objects = []
+objects.append(obj1)
+objects.append(obj2)
+
+#-------------вызов функции---------------
+scene_display(screen, objects, [20, 25], [width, height], 10, (255, 255, 255))
+# screen
+# objects - массив с экземплярами класса, имеющего атрибуты .x .y .r .color
+# [20, 25] - коордиаты камеры, не обязательно целые
+# [width, height] - размеры поля для рисования
+# 10 - zoom (размер одного пикселя (в экранных пикселях))
+# (255, 255, 255) - цвет фона
+
+# scene_display(screen, objects, [camera_x_cord, camera_y_cord], [screen_width, screen_height], zoom, background_color)
+'''
+
 if __name__ == "__main__":
     main()
