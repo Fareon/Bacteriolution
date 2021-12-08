9e5bdeeb525acc791d8324da3d72c3e339929678
valeryparfenov <parfenov.vr@phystech.edu>
2021-12-08 22:59:06 +0300

    Начало работы со звуком

---
+++ sound.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+pg.init()
+pg.mixer.music.load('sounds/background_music.wav')
+eat_food_sound = pg.mixer.Sound('sounds/eat_food_sound.wav')
+eat_cell_sound = pg.mixer.Sound('sounds/eat_cell_sound.wav.ogg')
+
+def play_background_music():
+    pg.mixer.music.play(-1)
+
+def play_eat_food_sound():
+    eat_food_sound.play()
+
+def play_eat_cell_sound():
+    eat_cell_sound.play()
