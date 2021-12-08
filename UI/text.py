9c0539d1f18f3ae9fa8caf321433b7faabcf548a
Yegor Tuchkov <tuchkov.e@phystech.edu>
2021-12-08 21:25:51 +0300

    text ui

---
+++ UI/text.py
@@ -0,0 +1,11 @@
+{
+     "#your_cell":
+     {
+
+         "font":
+         {
+             "size": "60",
+             "regular_path": "Pixeboy-z8XGD.ttf"
+        }
+     }
+}
--- ui.py
+++ ui.py
@@ -4,14 +4,13 @@ import game_manager as gm
 
 #ui variables storage
 
-info_panel_button = object
-manager = object
-
 manager = pygame_gui.UIManager((gm.screen_width, gm.screen_height), "UI/layout.json")
 
-info_panel_button = pygame_gui.elements.UIButton(relative_rect=pg.Rect((0, -10), (gm.ui_panel_width, gm.screen_height*1.2)),
+info_panel_button = pygame_gui.elements.UIButton(relative_rect=pg.Rect((-20, -20), (gm.ui_panel_width, gm.screen_height*1.2)),
                                                  text='', object_id=f"#info_panel",
                                                  manager=manager)
-info_panel_button2 = pygame_gui.elements.UIButton(relative_rect=pg.Rect((0, -10), (gm.ui_panel_width, gm.screen_height*1.2)),
-                                                 text='', object_id=f"#info_panel",
+your_cell_text = pygame_gui.elements.UILabel(relative_rect=pg.Rect((0, -10), (gm.ui_panel_width*0.9, gm.screen_height*0.2)),
+                                                 text ='YOUR CELL', 
+                                                 object_id=f"#label", 
+                                                 parent_element = info_panel_button,
                                                  manager=manager)
