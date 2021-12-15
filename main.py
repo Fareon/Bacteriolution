b8c631c0a15e2c69233e3ba9db9a8ab581323b88
Daniel <bdfaraon@gmail.com>
2021-12-15 10:31:41 +0300

    Refactor commit

--- main.py
+++ main.py
@@ -15,22 +15,25 @@ playing = True
 
 def handle_events(events):
     global alive
-    
+
     for event in events:
         if event.type == pg.USEREVENT:
-             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                 if event.ui_element == ui.info_panel_button:
-                     gm.ui_click = True
-                     print('Iteracted with ui')
-    
-    for event in events:                    
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == ui.info_panel_button:
+                    if ui.mutate_button.process_event(event):
+                        print('intended to mutate')
+                    gm.ui_click = True
+                    print('Iteracted with ui')
+
+    for event in events:
         ui.manager.process_events(event)
+
         if event.type == pg.QUIT:
             alive = False
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 sound.shut_down_music()
-                alive = False                
+                alive = False
                 print('Exited game')
             '''if event.key == pg.K_x:
                 print('cells:', len(gc.cells))
@@ -41,50 +44,53 @@ def handle_events(events):
 
         if event.type == pg.MOUSEBUTTONDOWN:
             global playing
-            if(playing == False):
+            if not playing:
                 playing = True
-                gc.generate_level(food_gens = 9, cells = 10, self_cells = 2)   
-            
-            if(pg.mouse.get_pos()[0] > gm.ui_panel_width):
+                gc.generate_level(food_gens=9, cells=10, self_cells=2)
+
+            if pg.mouse.get_pos()[0] > gm.ui_panel_width:
                 gm.clickpos = pg.mouse.get_pos()
                 gm.last_clicked_camera_pos = gm.camera_pos
             # print(gm.ScreenToScene(gm, gm.clickpos))
-            if(event.button == 4):
+            if event.button == 4:
                 gm.do_zoom(gm, +1)
-            if (event.button == 5):
+            if event.button == 5:
                 gm.do_zoom(gm, -1)
     gm.ui_click = False
 
-def check_win_condition(screen, screen_size = [gm.screen_width, gm.screen_height]):
+
+def check_win_condition(screen, screen_size=None):
+    if screen_size is None:
+        screen_size = [gm.screen_width, gm.screen_height]
     global playing
-    if(len(gc.self_cells) == 0):
+    if (len(gc.self_cells) == 0):
         print('no player cells left')
         scene.show_defeat_screen(screen, screen_size)
         playing = False
-    if(len(gc.cells) == 0):
+    if (len(gc.cells) == 0):
         print('no AI cells left')
         scene.show_victory_screen(screen, screen_size)
         playing = False
 
+
 def main():
     pg.init()
-    
+
     pg.display.set_caption('Bacteriolution')
     pg.font.init()
 
     screen = pg.display.set_mode((gm.screen_width, gm.screen_height))
-    
-    gc.generate_level(food_gens = 9, cells = 1, self_cells = 2)   
-    
+
+    gc.generate_level(food_gens=9, cells=1, self_cells=2)
+
     while alive:
         handle_events(pg.event.get())
-        
-        if(ui.game_speed_scrbar.check_has_moved_recently()):
-            gm.Game_FPS = 50 + ui.game_speed_scrbar.scroll_position/98 * 65    #bad code
-            
+
+        if ui.game_speed_scrbar.check_has_moved_recently():
+            gm.Game_FPS = 50 + ui.game_speed_scrbar.scroll_position / 98 * 65  # bad code
             print(ui.game_speed_scrbar.scroll_position)
-        
-        #DETECT INPUT
+
+        # DETECT INPUT
         keys_pressed = pg.key.get_pressed()
 
         if keys_pressed[pg.K_LEFT] or keys_pressed[pg.K_a]:
@@ -101,11 +107,11 @@ def main():
         if keys_pressed[pg.K_x]:
             gm.do_zoom(gm, -1)
 
-        #must go through all events here additionaly, otherwise won't scroll continuesly
-        
+        # must go through all events here additionaly, otherwise won't scroll continuesly
+
         # CALCULATE NEW POS
         if gm.frame % (gm.FPS // gm.Game_FPS) == 0 and playing:
-            #any gamecore events happen here
+            # any gamecore events happen here
             for cell in gc.self_cells:
                 gc.eat_food(cell, gc.self_cells)
                 if gm.clickpos is not None:
@@ -113,8 +119,7 @@ def main():
                     cell.move(move_to, gc.grid)
             for cell in gc.cells:
                 gc.eat_food(cell, gc.cells)
-                cell.move(cell.think_ahead(gc.cells+gc.self_cells, gc.food_generators, gc.food), gc.grid)
-                
+                cell.move(cell.think_ahead(gc.cells + gc.self_cells, gc.food_generators, gc.food), gc.grid)
 
             for food_gen in gc.food_generators:
                 if f.chance(food_gen.rate):
@@ -125,10 +130,12 @@ def main():
 
         scene.draw_sqare_objects(screen, gc.food, gm.camera_pos, [gm.screen_width, gm.screen_height],
                                  gm.zoom)  # draw food
-        scene.draw_sqare_objects(screen, gc.cells+gc.self_cells, gm.camera_pos, [gm.screen_width, gm.screen_height], gm.zoom) #draw cells
+        scene.draw_sqare_objects(screen, gc.cells + gc.self_cells, gm.camera_pos, [gm.screen_width, gm.screen_height],
+                                 gm.zoom)  # draw cells
         scene.draw_borders(screen, [gm.screen_width, gm.screen_height], gm.zoom,
-                     [gm.scene_width, gm.scene_height], color.random_color(), gm.borders_width, gm.camera_pos)
-        scene.draw_cross_objects(screen, gc.food_generators, gm.camera_pos, [gm.screen_width, gm.screen_height], gm.zoom) #draw foodgens
+                           [gm.scene_width, gm.scene_height], color.random_color(), gm.borders_width, gm.camera_pos)
+        scene.draw_cross_objects(screen, gc.food_generators, gm.camera_pos, [gm.screen_width, gm.screen_height],
+                                 gm.zoom)  # draw foodgens
 
         # еду рисуем при помощи той же функции, что и клетки
 
@@ -142,14 +149,14 @@ def main():
         # gamefield_size = [gamefield_width, gamefield_height] - размеры игрового поля, где может находиться клетка
         # borders_width - толщина границы в наших пикселях
 
-        
         ui.manager.update(1.0 / gm.Game_FPS)
         ui.manager.draw_ui(screen)
-        
+
+
         ui.draw_text(screen)
-        #scene.show_defeat_screen(screen, [gm.screen_width, gm.screen_height] )
-        check_win_condition(screen, screen_size = [gm.screen_width, gm.screen_height])
-        
+        # scene.show_defeat_screen(screen, [gm.screen_width, gm.screen_height] )
+        check_win_condition(screen, screen_size=[gm.screen_width, gm.screen_height])
+
         pg.display.update()
         time.sleep(1.0 / gm.Game_FPS)
         gm.frame += 1
